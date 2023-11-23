84f161a800c7d134b9cd2fe9cc6cfea3d5777667
aroyer <adrien.royer140203@gmail.com>
2023-11-23 11:17:03 +1100

    exam

--- contact/contact.php
+++ contact/contact.php
@@ -1,11 +1,27 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact</title>
 </head>
 <body>
-    
+
+    <h1>Contactez-nous</h1>
+
+    <form action="envoi.php" method="POST">
+        <div>
+            <label for="objet">Objet :</label>
+            <input type="text" id="objet" name="objet" required>
+        </div>
+        <div>
+            <label for="description">Description :</label>
+            <textarea id="description" name="description" rows="4" required></textarea>
+        </div>
+        <div>
+            <input type="submit" value="Envoyer">
+        </div>
+    </form>
+
 </body>
 </html>
